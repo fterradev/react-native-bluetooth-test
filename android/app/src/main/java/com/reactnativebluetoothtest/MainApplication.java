aba40e3e0920c04e240b4a989d15e6ec744cbcd3
Fernando Terra <nandobrt@gmail.com>
2018-03-02 12:24:06 -0300

    feat: Replace Bluetooth package version

--- android/app/src/main/java/com/reactnativebluetoothtest/MainApplication.java
+++ android/app/src/main/java/com/reactnativebluetoothtest/MainApplication.java
@@ -3,12 +3,12 @@ package com.reactnativebluetoothtest;
 import android.app.Application;
 
 import com.facebook.react.ReactApplication;
+import com.rctunderdark.NetworkManagerPackage;
 import com.facebook.react.ReactNativeHost;
 import com.facebook.react.ReactPackage;
 import com.facebook.react.shell.MainReactPackage;
 import com.facebook.soloader.SoLoader;
 
-import com.rctunderdark.NetworkManagerPackage;
 
 import java.util.Arrays;
 import java.util.List;
@@ -24,7 +24,8 @@ public class MainApplication extends Application implements ReactApplication {
     @Override
     protected List<ReactPackage> getPackages() {
       return Arrays.<ReactPackage>asList(
-          new MainReactPackage(), new NetworkManagerPackage()
+          new MainReactPackage(),
+            new NetworkManagerPackage()
       );
     }
 
