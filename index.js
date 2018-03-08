186e2220a07676fc82f16c31324c96bb133ad45d
Fernando Terra <nandobrt@gmail.com>
2018-03-08 12:00:29 -0300

    feat: Hide debugger's yellow box

--- index.js
+++ index.js
@@ -1,3 +1,6 @@
 import { AppRegistry } from 'react-native';
 import App from './App';
 AppRegistry.registerComponent('reactnativebluetoothtest', () => App);
+if (process.env.NODE_ENV === 'development') {
+  console.disableYellowBox = ['Remote debugger'];
+}
