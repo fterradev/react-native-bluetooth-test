7002e8251ca5ef1d08be5b3182cb931c999bba56
Fernando Terra <nandobrt@gmail.com>
2018-03-08 15:40:06 -0300

    feat: Ignore warning about "Debugger and device times (...)"

--- index.js
+++ index.js
@@ -2,5 +2,6 @@ import { AppRegistry } from 'react-native';
 import App from './App';
 AppRegistry.registerComponent('reactnativebluetoothtest', () => App);
 if (process.env.NODE_ENV === 'development') {
-  console.disableYellowBox = ['Remote debugger'];
+  //console.disableYellowBox = true;
+  console.ignoredYellowBox = ['Debugger and device times'];
 }
